215791c94af3dee45af2a60cb044adb64eb08e76
Rodney Canlas <canlasrj@gmail.com>
2021-03-05 14:02:56 -0800

    Implemented the car form within CustomerCarForm component and working on using loop logic in Inventory component.

--- Car-Dealership-App/ClientApp/src/components/CustomerCarForm.js
+++ Car-Dealership-App/ClientApp/src/components/CustomerCarForm.js
@@ -3,19 +3,46 @@ import React, { Component } from 'react';
 export class CustomerCarForm extends Component {
   static displayName = CustomerCarForm.name;
 
-  constructor(props) {
-    console.log("Constructor START");
-    super(props);
-    this.state = {
-        value: ''
-      };
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  state = {
+    car: {}
+  };
+
+  handleInputChange = (event) => {
+    event.persist();
+    this.setState(prevState => ({
+      car: {
+        ...prevState.car,
+        [event.target.name]: event.target.value
+      }
+    }))
+    console.log(this.state.car)
+  }
+
+  submitCar = (event) => {
+    event.preventDefault();
+    console.log(this.state.car);
   }
 
   render() {
     return (
       <div>
-        <form>
-            
+        <h1>Input Car Information</h1>
+        <form onSubmit={this.submitCar}>
+          <label>Owner: </label>
+          <input type="input" name="owner" placeholder="owner" onChange={this.handleInputChange}></input> 
+          <label>Make: </label>
+          <input name="make" placeholder="make" onChange={this.handleInputChange}></input>
+          <label>Model: </label>
+          <input name="model" placeholder="model" onChange={this.handleInputChange}></input>
+          <label>Year: </label>
+          <input name="year" placeholder="year" onChange={this.handleInputChange}></input>
+          <label>Color: </label>
+          <input name="color" placeholder="color" onChange={this.handleInputChange}></input>
+          <button type="submit">Submit</button>
         </form>
       </div>
     );
--- Car-Dealership-App/ClientApp/src/components/Inventory.js
+++ Car-Dealership-App/ClientApp/src/components/Inventory.js
@@ -20,19 +20,35 @@ export class Inventory extends Component {
 
   intitializeTestData()
   {
-    let carOne = {make: 'Cadillac', model: 'DeVille', year: 1998, owner: 'Todd'};
-    let carTwo = {make: 'BMW', model: '325Ci', year: 2002, owner: 'Bill'};
+    let carOne = {make: 'Cadillac', model: 'DeVille', year: 1998, owner: 'Todd', color: 'Green'};
+    let carTwo = {make: 'BMW', model: '325Ci', year: 2002, owner: 'Bill', color: 'White'};
     this.setState({
       listedCars: this.state.listedCars.push(carOne, carTwo),
       currentCount: this.state.listedCars.length
     });
   }
 
+  listCars = () => {
+    // for (let i = 0; i < this.state.listedCars.length; i++) {
+    //   <div>
+    //     <p>Car: {i}</p>
+    //     <ul>
+    //       <li>Owner: {this.state.listedCars[i].owner}</li>
+    //       <li>Make: {this.state.listedCars[i].make}</li>
+    //       <li>Model: {this.state.listedCars[i].model}</li>
+    //       <li>Year: {this.state.listedCars[i].year}</li>
+    //       <li>Color: {this.state.listedCars[i].color}</li>
+    //     </ul>
+    //   </div>
+    // }
+  }
+
   render() {
     return (
       <div>
         <h1>Inventory</h1>
-        <p aria-live="polite">Listed Cars: <strong>{this.state.currentCount}</strong></p>        
+        <p aria-live="polite">Listed Cars: <strong>{this.state.currentCount}</strong></p> 
+        {this.listCars()}     
       </div>
     );
   }
