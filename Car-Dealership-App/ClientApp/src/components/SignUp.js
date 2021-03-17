090ef378942eb1172ec624b78adb32218112cc3c
Rodney Canlas <canlasrj@gmail.com>
2021-03-17 13:30:54 -0700

    Implemented registration functionality part 2.

--- Car-Dealership-App/ClientApp/src/components/SignUp.js
+++ Car-Dealership-App/ClientApp/src/components/SignUp.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import { connect } from "react-redux";
-import { Container, Card, Form, Button,  Row, Col } from 'react-bootstrap'
+import { Container, Card, Form, Button, Row, Col } from 'react-bootstrap'
 
 
 export class SignUp extends Component {
@@ -9,8 +9,11 @@ export class SignUp extends Component {
   state = {
     firstName: "",
     lastName: "",
-    userName: "",
-    password: ""
+    username: "",
+    password: "",
+    role: "",
+    validUsername: false,
+    errorArea: "",
   };
 
   handleInputChange = (event) => {
@@ -20,12 +23,62 @@ export class SignUp extends Component {
     });
   }
 
+  handleRegister = () => {
+    const existingUsernames = this.getUsernames();
 
-  render() {
-    console.log("Inside render of SignUp");
+    if (!existingUsernames.includes(this.state.username)) {
+      this.setState({
+        validUsername: true
+      });
+
+      const first = this.state.firstName.toLowerCase();
+      const last = this.state.lastName.toLowerCase();
+
+      if (first === "laura" && last === "admin") {
+        this.setState({
+          role: "admin"
+        });
+      } else {
+        this.setState({
+          role: "user"
+        })
+      }
+
+      const newUser = {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        username: this.state.username,
+        password: this.state.password,
+        role: this.state.role
+      };
+
+      this.postUser(newUser);
+    } else {
+      console.log("inside handleRegister, else case")
+      this.setState({
+        errorArea: "This username is already chosen."
+      });
+
+    }
+    console.log("done with registering");
+  }
+
+  getUsernames = () => {
+    // make api call here
+    return ["animalLover", "hikingLover"];
+  }
+
+  postUser = (newUser) => {
+    //make api call here
+    console.log(newUser.toString());
+  }
+  
+
+  displayForm = () => {
     return (
-      <>
+    <>
         <p>Inside sign up page</p>
+        {this.state.errorArea}
         <Container>
           <Row>
             <Col>
@@ -33,7 +86,7 @@ export class SignUp extends Component {
                 <Card.Header>Sign Up Page</Card.Header>
                 <Card.Body>
                   <Form>
-                  <Form.Control type="input" placeholder="first name"
+                    <Form.Control type="input" placeholder="first name"
                       onChange={e => this.handleInputChange(e)}
                       name="firstName" />
                     <br></br>
@@ -43,7 +96,7 @@ export class SignUp extends Component {
                     <br></br>
                     <Form.Control type="input" placeholder="username"
                       onChange={e => this.handleInputChange(e)}
-                      name="userName" />
+                      name="username" />
                     <br></br>
                     <Form.Control type="input" placeholder="password"
                       onChange={e => this.handleInputChange(e)}
@@ -58,8 +111,15 @@ export class SignUp extends Component {
           </Row>
         </Container>
       </>
+    )
+  }
 
-
+  render() {
+    console.log("Inside render of SignUp")
+    return (
+      <>
+        {!this.state.validUsername && this.displayForm()}
+      </>
     );
   }
 }
