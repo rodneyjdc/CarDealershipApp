fc0d333294910676e17b1bb8be44d59d2168147c
Fang He <fanghe3@yahoo.com>
2021-03-22 15:20:16 -0400

    conection for mongoDb

--- Car-Dealership-App/ClientApp/src/components/CustomerCarForm.js
+++ Car-Dealership-App/ClientApp/src/components/CustomerCarForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Form, Button } from 'react-bootstrap'
 import { connect } from "react-redux";
+
+
 class CustomerCarForm extends Component {
   static displayName = CustomerCarForm.name;
 
@@ -8,19 +10,36 @@ class CustomerCarForm extends Component {
     car: {},
     formSubmitted: false,
     // acceptanceRules: {
-    //   year: 
+    //   year:
     // }
+  };
+
+  //state = { selectedFile: null };
+
+  fileChangedHandler = (event) => {
+   event.persist();
+   this.setState((prevState) => ({
+     car: {
+       ...prevState.car,
+       [event.target.name]: event.target.files[0],   
+     },
+   }));
+  };
+
+  uploadHandler = () => {
+    axios.post("my-domain.com/file-upload", this.state.car.image);
+    //console.log(this.state.selectedFile);
   };
 
   handleInputChange = (event) => {
     event.persist();
-    this.setState(prevState => ({
+    this.setState((prevState) => ({
       car: {
         ...prevState.car,
-        [event.target.name]: event.target.value
-      }
-    }))
-  }
+        [event.target.name]: event.target.value,
+      },
+    }));
+  };
 
   submitCar = (event) => {
     event.preventDefault();
@@ -31,28 +50,61 @@ class CustomerCarForm extends Component {
     // } else {
     //   alert("Input is not valid.");
     // }
-    
+
     this.props.addCar(this.state.car);
-  }
+  };
 
   render() {
-
-    console.log( this.props.username)
+    console.log(this.props.username);
     console.log(this.props);
     return (
       <div>
         <h2>Car Form {this.props.username}</h2>
         <Form onSubmit={this.submitCar}>
           <Form.Label>Owner: </Form.Label>
-          <Form.Control type="input" name="owner" placeholder="owner" onChange={this.handleInputChange}/>
+          <Form.Control
+            type="input"
+            name="owner"
+            placeholder="owner"
+            onChange={this.handleInputChange}
+          />
           <Form.Label>Make: </Form.Label>
-          <Form.Control type="input"name="make" placeholder="make" onChange={this.handleInputChange} />
+          <Form.Control
+            type="input"
+            name="make"
+            placeholder="make"
+            onChange={this.handleInputChange}
+          />
           <Form.Label>Model: </Form.Label>
-          <Form.Control type="input" name="model" placeholder="model" onChange={this.handleInputChange} />
+          <Form.Control
+            type="input"
+            name="model"
+            placeholder="model"
+            onChange={this.handleInputChange}
+          />
           <Form.Label>Year: </Form.Label>
-          <Form.Control type="input" name="year" placeholder="year" onChange={this.handleInputChange} />
+          <Form.Control
+            type="input"
+            name="year"
+            placeholder="year"
+            onChange={this.handleInputChange}
+          />
           <Form.Label>Color: </Form.Label>
-          <Form.Control type="input" name="color" placeholder="color" onChange={this.handleInputChange} />
+          <Form.Control
+            type="input"
+            name="color"
+            placeholder="color"
+            onChange={this.handleInputChange}
+          />
+          <Form.Label>Image: </Form.Label>
+          <Form.Control
+            type="file"
+            name="image"
+            placeholder="image"
+            onChange={this.fileChangedHandler}
+          />
+          <Button onClick={this.uploadHandler}>Upload!</Button>
+
           <br></br>
           <Button type="submit">Submit</Button>
         </Form>
