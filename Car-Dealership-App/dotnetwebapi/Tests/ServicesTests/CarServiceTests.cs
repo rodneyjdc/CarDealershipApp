70aef010bd12c87293a6ecadba737af4980c1db2
Rodney Canlas <canlasrj@gmail.com>
2021-04-01 12:23:04 -0700

    Writing unit tests for dotnet webapi services.

--- Car-Dealership-App/dotnetwebapi/Tests/ServicesTests/CarServiceTests.cs
+++ Car-Dealership-App/dotnetwebapi/Tests/ServicesTests/CarServiceTests.cs
@@ -7,33 +7,28 @@ using dotnetwebapi.Services;
 
 namespace ServicesTests
 {
-    public class CarServiceTests
+    public class CarServiceTests : ServiceTest
     {
+        public CarServiceTests()
+                : base(
+                    new DbContextOptionsBuilder<CarDealershipDbContext>()
+                        .UseSqlite("Data Source=C:\\Users\\Rodney\\Desktop\\LTI Resources\\training projects\\course-demos\\03302021\\EFGetStarted\\CarDealershipTestDB.db")
+                        .Options)
+        {
+        }
+
         [Fact]
-        public void GetAllTest()
+        public void Can_Get_All_Cars()
         {
-            var options = new DbContextOptionsBuilder<CarDealershipDbContext>()
-            .UseInMemoryDatabase(databaseName: "CarDealershipDatabase")
-            .Options;
+            using (var context = new CarDealershipDbContext(ContextOptions))
+            {
+                var carService = new CarService(context);
 
-        // Insert seed data into the database using one instance of the context
-        using (var context = new CarDealershipDbContext(options))
-        {
-            context.Cars.Add(new Car {CarId = 1, Name = "Car 1", Price = 2018, Location = "Action", Seller = "Valentin", Image = "", Date = new DateTime().Now });
-            context.Cars.Add(new Car {CarId = 2, Name = "Car 2", Price = 6000, Location = "Maryland", Seller = "Vandit", Image = "", Date = new DateTime().Now });
-            context.Cars.Add(new Car {CarId = 3, Name = "Car 3", Price = 1245, Location = "Italia", Seller = "Rodney", Image = "", Date = new DateTime().Now });
-            context.SaveChanges();
+                var cars = carService.GetAll();
+
+                Assert.Equal(4, cars.Count);
+            }
         }
 
-        // Use a clean instance of the context to run the test
-        using (var context = new CarDealershipDbContext(options))
-        {
-            CarService carRepository = new CarService(context);
-            List<Cars> cars = CarService.GetAll();
-
-            Assert.Equal(3, cars.Count);
-        }
-
-        }
     }
 }
